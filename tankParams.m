6efc2504d1e128039b1b2da0a28aa67d37d9ede0
Tom Ng <tigrasngovn111@gmail.com>
2026-03-02 12:19:20 +0700

    Update tankParams.m

--- tankParams.m
+++ tankParams.m
@@ -1,8 +1,8 @@
-hinit=10; %cm
-hmax=50; %cm
-Amax=200; %cm^2
-Amin=100; %cm^2
-a=1; %cm^2
-k=300;
-cd=0.6;
-g=981; %cm/s^2
+hinit = 10; % Initial height - cm
+hmax = 50; % Maximum height - cm
+Amax = 200; % Maximum cross sectional area - cm^2
+Amin = 100; % Minimum cross sectional area - cm^2
+a = 1; % Orifice area - cm^2
+k = 300; % Pump power coefficient
+cd = 0.6; % Discharge coefficient
+g = 981; % Gravitational acceleration - cm/s^2
